39e8eb137f7a8df0cde02eaa6052643b2077c645
Manifest Bumper <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-21 15:39:00 +0000

    chore: bump manifest version to v1.0.5

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,7 +5,7 @@ game 'gta5'
 
 description "Advanced Banking System - LB Phone integration"
 author "Felis Development"
-version      '1.0.4'
+version      '1.0.5'
 repository 'https://github.com/felisDevelopment/fd_banking_app'
 
 dependencies {
